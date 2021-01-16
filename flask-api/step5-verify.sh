ee7442abcec0bae83e5bb86c22d98166490c3ca4
Richard Lui <cswclui@polyu.edu.hk>
2021-01-16 20:08:32 +0800

    'c3'

--- flask-api/step5-verify.sh
+++ flask-api/step5-verify.sh
@@ -2,15 +2,7 @@ myname=`curl -s http://localhost:5000/me| jq  '.name' |tr -d '"'`
 myid=`curl -s http://localhost:5000/me| jq  '.id' | tr -d '"'`
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 
-result=``
-
-curl -s http://localhost:5000/books/0 |jq ''.nam
-
-Python programming (2nd Ed)
-
-[[ $myname != "put your name" ]] && [[ $myid != "put your studentID" ]]   &&
-curl -s -G --data-urlencode "type=flask-step1-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "not completed"
 
 
-# reporting url
-# https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
+[[ $myname != "put your name" ]] && [[ $myid != "put your studentID" ]]   && python verify.py && curl -s -G --data-urlencode "type=flask-step1-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "not completed"
+
