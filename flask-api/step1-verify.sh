8ff970b2de4ef97d1f2c4329ceaea752c292999f
Richard Lui <cswclui@polyu.edu.hk>
2021-01-16 11:02:25 +0800

    'c3'

--- flask-api/step1-verify.sh
+++ flask-api/step1-verify.sh
@@ -1,3 +1,3 @@
-[[ $(curl -s http://localhost:5000/me| jq  '.name'| tr -d '"') != "put your name" ]] && [[ $(curl -s http://localhost:5000/me| jq  '.id'| tr -d '"') != "put your studentID" ]] && echo "done" || echo "not done"
+[[ $(curl -s http://localhost:5000/me| jq  '.name'| tr -d '"') != "put your name" ]] && [[ $(curl -s http://localhost:5000/me| jq  '.id'| tr -d '"') != "put your studentID" ]] && echo "done" || echo "not completed"
 
 
