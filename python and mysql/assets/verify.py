8d68b20e526ba3a33860ba0fd72d15897ba11f37
Richard Lui <cswclui@polyu.edu.hk>
2021-01-18 21:25:10 +0800

    test

--- python and mysql/assets/verify.py
+++ python and mysql/assets/verify.py
@@ -1,10 +1,31 @@
-import os
+import os,requests
+
+event_type='python-sql-step3-ex'
+myname='test'
+myid='123'
+
+
+
 stream = os.popen('python3 interest.py')
 stream2 = os.popen('cat expected.txt')
 output1 = stream.read()
 output2 = stream2.read()
 if output1 == output2:
-    print("done")
+    try:
+        f1 = open("myname", "r")
+        f2 = open("myid", "r")   
+        myname=f1.read().strip()
+        myid=f2.read().strip()
+        print(myname)
+        print(myid)
+        
+        event_type='python-sql-step3-ex'
+        
+        url=f"https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1?type={event_type}&myid={myid}&name={myname}"
+        print(url)
+        r = requests.get(url) 
+        print(r)
+    except Exception as e:
+        print(e)
 else:
     print("different")
-
