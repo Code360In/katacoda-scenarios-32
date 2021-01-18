0f40637763671980e2c214e26bdf045c5e2977db
Richard Lui <cswclui@polyu.edu.hk>
2021-01-18 16:26:56 +0800

    test

--- basic-docker/inputname.sh
+++ basic-docker/inputname.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-echo "Please input your studentID:"
-read myname
 
-echo "Please input your studentName:"
-read myid
+if [ ! -f "myname" ]
+then
+	echo "Please input your student ID:"
+	read myname
+	echo $myname > myname
+fi
 
+if [ ! -f "myid" ]
+then
+	echo "Please input your studentName:"
+	read myid
+	echo $id > myid
+fi
 
