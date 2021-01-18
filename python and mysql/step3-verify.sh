12e8e39bd4635e39a0f9d170abebf1035c621d78
Richard Lui <cswclui@polyu.edu.hk>
2021-01-18 18:05:28 +0800

    test

--- python and mysql/step3-verify.sh
+++ python and mysql/step3-verify.sh
@@ -1,13 +1,13 @@
-url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
+url='https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1'
 event_type='python-sql-step3-ex'
 myname=`cat myname`
 myid=`cat myid`
 
-echo "start2">start8
+echo "start2">start10
 
 [[ $myname != "" ]] && [[ $myid != "" ]]   && [[ `python3 interest.py | diff -w expected.txt -` == '' ]]  && curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" && echo "done" ||echo "not yet!"
 
 
-# && curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null 
+
 
 
