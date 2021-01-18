ccdd9824f1dc71f7e50b56178044280139308172
Richard Lui <cswclui@polyu.edu.hk>
2021-01-18 15:54:11 +0800

    test

--- flask-api/step5-verify.sh
+++ flask-api/step5-verify.sh
@@ -3,5 +3,5 @@ myid=`curl -s http://localhost:5000/me| jq  '.id' | tr -d '"'`
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 
 
-[[ $myname != "put your name" ]] && [[ $myid != "put your studentID" ]]   && python /v/verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "not completed"
+[[ $myname != "put your name" ]] && [[ $myid != "put your studentID" ]]   && python verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "not completed"
 
