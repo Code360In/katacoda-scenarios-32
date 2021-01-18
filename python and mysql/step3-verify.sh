23427466ccb597927a097ab6c96a09afcebe4e93
Richard Lui <cswclui@polyu.edu.hk>
2021-01-18 20:18:10 +0800

    test

--- python and mysql/step3-verify.sh
+++ python and mysql/step3-verify.sh
@@ -3,7 +3,7 @@ event_type='python-sql-step3-ex'
 myname=`cat myname`
 myid=`cat myid`
 
-echo "start2">start31
+echo "start2">start32
 
 python3 interest.py > actual.txt
 
@@ -12,14 +12,3 @@ diff out.txt expected.txt> diff.txt
 DIFF=`cat diff.txt`
 
 echo $DIFF > out2.txt
-
-if [ -f expected.txt ] && [ $DIFF == "" ] 
-then
-    echo "interest  correct"
-	echo "interest.py correct">interest_ok
-	[[ $myname != "" ]] && [[ $myid != "" ]]    && curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"
-else
-	echo "interest not correct"
-	echo "interest.py correct">interest_not_ok
-fi
-
