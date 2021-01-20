c2f0bd1177e16942b9c212a5e538bca14c93f6f1
Richard Lui <cswclui@polyu.edu.hk>
2021-01-21 00:29:35 +0800

    test

--- flask-api/step1-verify.sh
+++ flask-api/step1-verify.sh
@@ -3,5 +3,6 @@ myid=`curl -s http://localhost:5000/me| jq  '.id' | tr -d '"'`
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 
 [[ $myname != "" ]] && [[ $myid != "" ]]   &&
-curl -s -G --data-urlencode "type=flask-step1-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "not completed"
+curl -s -G --data-urlencode "type=flask-step1-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"|| echo "You should start your flask server and specify your ID and name in the python script"
+
 
