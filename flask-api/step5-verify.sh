c3941d6a1592a3b73fd75a6764ffd9323ca33ff7
Richard Lui <cswclui@polyu.edu.hk>
2021-01-21 00:51:16 +0800

    test

--- flask-api/step5-verify.sh
+++ flask-api/step5-verify.sh
@@ -3,5 +3,5 @@ myid=`curl -s http://localhost:5000/me| jq  '.id' | tr -d '"'`
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 
 
-[[ $myname != "" ]] && [[ $myid != "" ]]   && python .verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"||  echo "Please add an API endpoint books/<book_id> for updating a given book. Submit HTTP PUT request to update the first book as shown in the instruction." >error.log
+[[ $myname != "" ]] && [[ $myid != "" ]]   && python .verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"||  echo "not yet"
 
