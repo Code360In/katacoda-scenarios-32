e0d96ed019d54e8a53c57a9f0d264086d5f8ba3d
Richard Lui <cswclui@polyu.edu.hk>
2021-01-21 00:54:19 +0800

    test

--- flask-api/step5-verify.sh
+++ flask-api/step5-verify.sh
@@ -3,6 +3,6 @@ myid=`curl -s http://localhost:5000/me| jq  '.id' | tr -d '"'`
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 
 
-[[ $myname != "" ]] && [[ $myid != "" ]]   && python .verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"||  echo "Please implement the HTTP PUT endpoint to update a book">error2.log
+[[ $myname != "" ]] && [[ $myid != "" ]]   && python .verify.py && curl -s -G --data-urlencode "type=flask-step5-ex" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null && echo "done"||  echo "Please implement the HTTP PUT endpoint to update a book" >error2.log
 
 
