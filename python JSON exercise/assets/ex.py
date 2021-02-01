8a7029b838318d184a90e6653a4dc29f33f4e4e0
Richard Lui <cswclui@polyu.edu.hk>
2021-02-01 19:48:15 +0800

    test

--- python JSON exercise/assets/ex.py
+++ python JSON exercise/assets/ex.py
@@ -7,12 +7,12 @@ f2 = open("products.json", mode="r", encoding="utf-8")
 
 orders = json.loads(f.read())
 product_list = json.loads(f2.read())
-user_list = json.loads(f3.read())
 
 
-print(orders)
-print(product_list)
-print(user_list)
+
+pprint(orders)
+pprint(product_list)
+
 
 #convert the product list to python dictionary
 catalog={}
@@ -22,13 +22,5 @@ for i in product_list:
     catalog[product_id]["price"]=float(i['price'])
     catalog[product_id]["title"]=i['title']
 
-#convert the user list to python dictionary
-users={}
-for i in user_list:
-    user_id=i['id']
-    
-    users[user_id]={}
-    users[user_id]['username']=i['username']
- 
 
 pprint(catalog)
