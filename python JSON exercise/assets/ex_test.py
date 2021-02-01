b67c9388ea9296e6fa682039bed0a607c631de01
Richard Lui <cswclui@polyu.edu.hk>
2021-02-01 22:27:56 +0800

    m1

--- python JSON exercise/assets/ex_test.py
+++ python JSON exercise/assets/ex_test.py
@@ -1,14 +1,16 @@
 import pytest
-import ex2,ex3,ex4
 
 
 def test_ex2():
+    import ex2
     assert ex2.f2(100) == 7   
     
 def test_ex3():
+    import ex3
     assert ex3.f3(101) == 20
     
 def test_ex4():
+    import ex4
     assert ex4.f4(1) ==775.74
     
     
