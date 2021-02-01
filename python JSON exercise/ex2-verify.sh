f108f12f22f4e7ae3ce55ddb20997460fb718262
Richard Lui <cswclui@polyu.edu.hk>
2021-02-01 23:00:14 +0800

    test

--- python JSON exercise/ex2-verify.sh
+++ python JSON exercise/ex2-verify.sh
@@ -1,3 +1,3 @@
-cp tests/ex_test.py /root
+cp /tests/ex_test.py /root
 pytest  -k test_ex2 >test.log && echo "done"
 rm /tests/ex_test.py
--- python JSON exercise/ex4-verify.sh
+++ python JSON exercise/ex4-verify.sh
@@ -1,3 +1,3 @@
-cp tests/ex_test.py /root
+cp /tests/ex_test.py /root
 pytest  -k test_ex4 >test.log && echo "done"
 rm /tests/ex_test.py
