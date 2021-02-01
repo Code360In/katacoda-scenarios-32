64a0f517d6f90e52bcefc342c0f2a92e5128159c
Richard Lui <cswclui@polyu.edu.hk>
2021-02-01 23:22:03 +0800

    test

--- python JSON exercise/ex2-verify.sh
+++ python JSON exercise/ex2-verify.sh
@@ -1,3 +1,18 @@
+
+url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
+event_type='python_json-ex2'
+myname=`cat myname`
+myid=`cat myid`
+
+if [[ $myname == "" ]] || [[ $myid == "" ]]
+then
+	exit 0
+fi
+
+
 cp /tests/ex_test.py /root
-pytest  -k test_ex2 >test.log && echo "done"
+
+pytest  -k test_ex2 >test.log && echo "done" && curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null
+
 rm /root/ex_test.py
+
