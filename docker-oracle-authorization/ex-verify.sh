2630b163fbbd086ccafece840af428eee4c96262
Richard Lui <cswclui@polyu.edu.hk>
2021-02-04 21:19:04 +0800

    test

--- docker-oracle-authorization/ex-verify.sh
+++ docker-oracle-authorization/ex-verify.sh
@@ -2,7 +2,6 @@
 docker container ls >out.txt
 
 
-
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 event_type='oracle-authorization-ex'
 myname=`cat .myname`
@@ -15,14 +14,20 @@ fi
 
 cat <<EOF >.script.sql
 SET HEADING OFF;
+SPOOL /home/oracle/test
 select GRANTEE, GRANTOR, PRIVILEGE,GRANTABLE from DBA_TAB_PRIVS where TABLE_NAME='T';
 exit;
 EOF
 
-docker cp .script.sql oracle-xe:/
+docker cp .script.sql oracle-xe:/home/oracle
 
-docker exec -it --user oracle  oracle-xe /opt/oracle/product/18c/dbhomeXE/bin/sqlplus -S / as sysdba @/.script.sql |  tr -d '[:space:]' >out2.txt
+#docker exec -it --user oracle  oracle-xe /opt/oracle/product/18c/dbhomeXE/bin/sqlplus -S / as sysdba @/.script.sql |  tr -d '[:space:]' >out2.txt
 
+docker exec -it --user oracle  oracle-xe /opt/oracle/product/18c/dbhomeXE/bin/sqlplus -S / as sysdba @/home/oracle/.script.sql 
+
+docker cp oracle-xe:/home/oracle/test.lst /root
+
+cat /root/test.lst |tr -d '[:space:]' >out2.txt
 
 if [ `cat out2.txt` != "U2U1SELECTYESU3U2SELECTNO" ] 
 then
