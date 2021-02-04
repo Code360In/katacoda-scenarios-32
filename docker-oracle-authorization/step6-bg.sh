0dd9efc78ef5b1c74d7e953cb83ed468dc228ef1
Richard Lui <cswclui@polyu.edu.hk>
2021-02-04 22:47:53 +0800

    test

--- docker-oracle-authorization/step6-bg.sh
+++ docker-oracle-authorization/step6-bg.sh
@@ -1,7 +1,7 @@
 
 url=https://a2pops3li6.execute-api.us-east-2.amazonaws.com/v1
 event_type='oracle-authorization-ex-start'
-myname=`Unknown`
-myid=`Unknown`
+myname="Unknown"
+myid="Unknown"
 
 curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null
