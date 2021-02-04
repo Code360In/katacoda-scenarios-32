a33586bf2d4ff250bc16388e4ba619162230b483
Richard Lui <cswclui@polyu.edu.hk>
2021-02-05 00:04:01 +0800

    test

--- docker-oracle-authorization/step6-bg.sh
+++ docker-oracle-authorization/step6-bg.sh
@@ -4,6 +4,6 @@ event_type='oracle-authorization-ex-start'
 myname="Unknown"
 myid="Unknown"
 
-curl -s -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >/dev/null
+curl -v -G --data-urlencode "type=${event_type}" --data-urlencode "myid=${myid}" --data-urlencode "name=${myname}" "$url" >.curl_log
 
 echo "exercise logged" >.logged
